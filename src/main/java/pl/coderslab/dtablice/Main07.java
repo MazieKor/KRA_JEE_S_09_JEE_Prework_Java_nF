593de6616cf588f01e6a0e9da90a836aed2eb6e2
MazieKor <maciej.coders@gmail.com>
2020-08-21 01:31:21 +0200

    Zamienienie skali temperatur z Celsjusza na Fahrenheita. Podanie średniej temperatur.

--- src/main/java/pl/coderslab/dtablice/Main07.java
+++ src/main/java/pl/coderslab/dtablice/Main07.java
@@ -4,6 +4,20 @@ public class Main07 {
 
 	public static void main(String[] args) {
 		double[] temps = {30, 29, 14, 42, -4, -10, 8, 14, 32, 11, 8, 0, 0};
+		for(int i = 0; i < temps.length; i++) {
+			temps[i] = temps[i] * 1.8 +32;
+		}
+		double sum = 0;
+		double avg;
+		for(int i = 0; i < temps.length; i++) {
+			sum += temps[i];
+		}
+		avg = sum / temps.length;
+		String result = String.format("%.2f", avg);
+		System.out.println("SREDNIA: " + avg);
+		System.out.println("SREDNIA SKRÓCONA: " + result);  //polecana w preworku metoda zaokrąglenia wywołuje błąd
+															//w teście (jesli użyć tylko niej do podania wyniku)
 	}
+
 
 }
