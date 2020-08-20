67144f50f9120448ade0a79c2cb4ae0815d14967
MazieKor <maciej.coders@gmail.com>
2020-08-20 18:49:47 +0200

    Stworzenie 2 pętli odliczających od zera do podanej z góry liczby

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main06.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main06.java
@@ -4,6 +4,26 @@ public class Main06 {
 
 	public static void main(String[] args) {
 		int n = 6;
+		int i = 0;
+		while (n >= i) {
+			System.out.print(i);
+			if (i % 2 == 0) {
+				System.out.println(" - parzysta");
+			}
+			else {
+				System.out.println(" - nieparzysta");
+			}
+			i++;
+		}
+		for (int j = 0; n >= j; j++) {
+			System.out.print(j);
+			if (j % 2 == 0) {
+				System.out.println(" - parzysta");
+			}
+			else {
+				System.out.println(" - nieparzysta");
+			}
+		}
 	}
 
 }
