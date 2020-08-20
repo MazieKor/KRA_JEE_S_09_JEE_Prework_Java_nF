ffe2350730464df1b8bf4d2bdf4190eb81faa19a
MazieKor <maciej.coders@gmail.com>
2020-08-20 17:40:17 +0200

    Dodanie instrukcji warunkowej if else

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main01.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main01.java
@@ -3,7 +3,17 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main01 {
 
     public static void main(String[] args) {
-
+        int nr1 = 3;
+        int nr2 = 7;
+        if (nr1 > nr2) {
+            System.out.println("Większa liczba to " + nr1);
+        }
+        else if (nr2 > nr1) {
+            System.out.println("Większa liczba to " + nr2);
+        }
+        else {
+            System.out.println("Liczby są sobie równe");
+        }
     }
 
 }
