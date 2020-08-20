f28cf5b2a39a0e2a9667a181086b2ed5939a28d2
MazieKor <maciej.coders@gmail.com>
2020-08-20 18:17:36 +0200

    Stworzenie 2 pętli wypisującej 10 pierwszych liczb naruralnych

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main04.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main04.java
@@ -3,6 +3,15 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main04 {
 
 	public static void main(String[] args) {
+		int i = 1;
+		while (i < 11) {
+			System.out.print(i + " ");
+			i++;
+		}
+		System.out.println("");
+		for (int j = 1; j <= 10; j++) {
+			System.out.print(j + " ");
+		}
 
 	}
 
