706c72c718cbe5ca003cb5a599773f8ee3f31487
MazieKor <maciej.coders@gmail.com>
2020-08-20 18:00:42 +0200

    Stworzenie pętli wypisującej 6 razy słowo Java

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main03.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main03.java
@@ -3,6 +3,14 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main03 {
 
 	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 3) {
+			System.out.println("Java");
+			i++;
+		}
+		for (int j = 1; j <=3; j += 1) {
+			System.out.println("Java");
+		}
 
 	}
 
