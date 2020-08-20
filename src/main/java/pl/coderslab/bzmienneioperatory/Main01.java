df678bf30c0231152a65298281cbc03a4dc159fe
MazieKor <maciej.coders@gmail.com>
2020-08-20 16:37:56 +0200

    Deklaracja i inicjalizacja zmiennych

--- src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
+++ src/main/java/pl/coderslab/bzmienneioperatory/Main01.java
@@ -3,7 +3,11 @@ package pl.coderslab.bzmienneioperatory;
 public class Main01 {
 
 	public static void main(String[] args) {
-
+		int int1 = 23;
+		char char2 = 'M';
+		long long3 = 3459869;
+		double double4 = 3.14;
+		boolean boolean5 = false;
 	}
 
 }
