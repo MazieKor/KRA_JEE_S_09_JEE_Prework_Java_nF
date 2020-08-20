6c3ede72dbe41097bea3070ae3f8f29f799c9042
MazieKor <maciej.coders@gmail.com>
2020-08-20 23:20:59 +0200

    Stworzenie tablicy 3-elementowej

--- src/main/java/pl/coderslab/dtablice/Main01.java
+++ src/main/java/pl/coderslab/dtablice/Main01.java
@@ -3,7 +3,13 @@ package pl.coderslab.dtablice;
 public class Main01 {
 
     public static void main(String[] args) {
-
+        int[] threeElements = new int[3];
+        threeElements[0] = 3;
+        threeElements[1] = 4;
+        threeElements[2] = 5;
+        System.out.print(threeElements[0] + " ");
+        System.out.print(threeElements[1] + " ");
+        System.out.print(threeElements[2] + " ");
     }
 
 }
