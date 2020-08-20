1e689da8d7680525deb739320a0a8e4eab5d2130
MazieKor <maciej.coders@gmail.com>
2020-08-20 20:53:05 +0200

    Stworzenie 2 pętli: Zagnieżdżonej zależnej i zagnieżdżonej niezależnej z dodaniem funkcjonalności if i break

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main09.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main09.java
@@ -4,5 +4,28 @@ public class Main09 {
 
     public static void main(String[] args) {
         int n = 5;
+        // Rozwiązanie za pomocą pętli zależnej:
+        for(int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();  //dodane w celu zrobienia odstępu od drugiego rozwiązania
+
+        // Rozwiązanie za pomocą pętli niezależnej (?):
+        for(int i = 0; i < n; i++) {
+            String row = "";
+            for(int j = 0; j < n; j++) {
+                if (j <= i) {
+                    row = row + "* ";
+                }
+                else {
+                    break;
+                }
+            }
+                System.out.println(row);
+
+        }
     }
 }
