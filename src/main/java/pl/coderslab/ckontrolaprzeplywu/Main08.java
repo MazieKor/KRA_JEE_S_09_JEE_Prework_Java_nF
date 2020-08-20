2080ccb2a40e35dfd53776c54dc0a9cebef20e3b
MazieKor <maciej.coders@gmail.com>
2020-08-20 20:07:04 +0200

    Stworzenie 2 pętli zagnieżdżonych.

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main08.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main08.java
@@ -4,5 +4,34 @@ public class Main08 {
 
     public static void main(String[] args) {
         int n = 5;
+        for(int i = 1; i <= n; i++) {
+            String result = "";
+            for(int j = 1; j <= n; j++) {
+                if (j <= i) {
+                    result += "* ";
+                }
+                else {
+                    result = result + j + " ";
+                }
+            }
+            System.out.println(result);
+        }
+
+        /* Inny sposób na osiągnięcie tego samego wyniku
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= n; j++) {
+                if (j == n) {
+                    System.out.println();
+                }
+                else if (j <= i) {
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print(j + 1 + " ");
+                }
+
+            }
+        }  */
+
     }
 }
