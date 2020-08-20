981ca4fd2013b98586c9f429b769c04cc1effda3
MazieKor <maciej.coders@gmail.com>
2020-08-20 18:33:56 +0200

    Stworzenie 2 pętli wypisujących sumę 10 pierwszych liczb naruralnych

--- src/main/java/pl/coderslab/ckontrolaprzeplywu/Main05.java
+++ src/main/java/pl/coderslab/ckontrolaprzeplywu/Main05.java
@@ -3,6 +3,18 @@ package pl.coderslab.ckontrolaprzeplywu;
 public class Main05 {
 
 	public static void main(String[] args) {
+		int resultFor = 0;
+		int resultWhile = 0;
+		int i = 0;
+		while (i < 11) {
+			resultWhile = resultWhile + i;
+			i++;
+		}
+		System.out.println(resultWhile);
+		for (int j = 0; j < 11; j++) {
+			resultFor = resultFor + j;
+		}
+		System.out.println(resultFor);
 
 	}
 
