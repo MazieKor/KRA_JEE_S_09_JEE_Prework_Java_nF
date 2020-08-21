1c6d59e74fd3633481839546a20ee980390ef411
MazieKor <maciej.coders@gmail.com>
2020-08-21 03:17:50 +0200

    Program sprawdzający możliwość budowy trójkąta z podanych parametrów startowych.

--- src/main/java/pl/coderslab/eparametrystartowe/Main04.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main04.java
@@ -3,6 +3,19 @@ package pl.coderslab.eparametrystartowe;
 public class Main04 {
 
     public static void main(String[] args) {
+        int prawLiczbArg = 3;
+        if(args.length != prawLiczbArg){
+            System.out.println("Błąd - proszę podaj dokładnie 3 boki");
+        }
+        else if (Integer.parseInt(args[0]) + Integer.parseInt(args[1]) > Integer.parseInt(args[2]) &&
+                Integer.parseInt(args[0]) + Integer.parseInt(args[2]) > Integer.parseInt(args[1]) &&
+                Integer.parseInt(args[1]) + Integer.parseInt(args[2]) > Integer.parseInt(args[0])
+                ) {
+            System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
 
     }
 
