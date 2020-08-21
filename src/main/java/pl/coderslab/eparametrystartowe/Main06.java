bcdf15880d92149392190fb89d53a446ed0a5510
MazieKor <maciej.coders@gmail.com>
2020-08-21 03:59:18 +0200

    Program przedstawiający tabiczkę mnożenia dla wybranej liczby parametra startowego.

--- src/main/java/pl/coderslab/eparametrystartowe/Main06.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main06.java
@@ -3,6 +3,15 @@ package pl.coderslab.eparametrystartowe;
 public class Main06 {
 
     public static void main(String[] args) {
+        int points = Integer.parseInt(args[0]);
+        int i = 1;
+        while (i <= points){
+            for (int j = 1; j <= points; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+        System.out.println();
+        i = i + 1;
+        }
 
     }
 
