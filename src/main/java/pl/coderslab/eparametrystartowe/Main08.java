813feaddeb19ad7a77590fecaf29b218e2dd82db
MazieKor <maciej.coders@gmail.com>
2020-08-21 05:29:02 +0200

    Program obliczający największy wspólny dzielnik.

--- src/main/java/pl/coderslab/eparametrystartowe/Main08.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main08.java
@@ -3,6 +3,27 @@ package pl.coderslab.eparametrystartowe;
 public class Main08 {
 
     public static void main(String[] args) {
+        double x = Integer.parseInt(args[0]);
+        double y = Integer.parseInt(args[1]);
+        double min;
+        double max[] = new double[10000];
+        int a = 0;
+        if (x > y) {
+            min = y;
+            max[a] = x;
+        }
+        else {
+            min = x;
+            max[a] = y;
+        }
+        while (max[a] % min > 0) {
+               max[a+1] = min;
+               min = max[a] % min;
+               a++;
+        }
+        System.out.println(x + " i " + y + ": " + min);
+
+
 
     }
 
