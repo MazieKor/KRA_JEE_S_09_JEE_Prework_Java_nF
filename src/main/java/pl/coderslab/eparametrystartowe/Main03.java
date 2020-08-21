52009007f6fa0f8aa093383909ba84bc9eadf357
MazieKor <maciej.coders@gmail.com>
2020-08-21 02:51:57 +0200

    Obliczenie i wypisanie średniej z parametrów startowych.

--- src/main/java/pl/coderslab/eparametrystartowe/Main03.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main03.java
@@ -2,6 +2,15 @@ package pl.coderslab.eparametrystartowe;
 
 public class Main03 {
     public static void main(String[] args) {
+        double sum = 0;
+        double avg;
+        for(int i = 0; i < args.length; i++){
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        avg = sum / args.length;
+        System.out.println(avg);
+        //System.out.println("SUMA PARAMETRÓW STARTOWYCH: " + sum);  <- zakomentowano ze względu na błędy w teście
+        //System.out.println("SREDNIA PARAMETRÓW STARTOWYCH: " + avg);
 
     }
 }
