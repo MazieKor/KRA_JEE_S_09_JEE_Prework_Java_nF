547ab7459a700a48dde71f021590488b6c1e6ab8
MazieKor <maciej.coders@gmail.com>
2020-08-21 03:33:53 +0200

    Program wystawiający ocenę an podstawie parametrów startowych.

--- src/main/java/pl/coderslab/eparametrystartowe/Main05.java
+++ src/main/java/pl/coderslab/eparametrystartowe/Main05.java
@@ -3,7 +3,22 @@ package pl.coderslab.eparametrystartowe;
 public class Main05 {
 
     public static void main(String[] args) {
-
+        int points = Integer.parseInt(args[0]);
+        if(points > 100 || points < 0) {
+            System.out.println("BADVALUE");
+        }
+        else if(points >= 0 && points <= 50) {
+            System.out.println("F");
+        }
+        else if (points > 50 && points <= 70) {
+            System.out.println("E");
+        }
+        else if (points > 70 && points <= 90) {
+            System.out.println("C");
+        }
+        else {
+            System.out.println("A");
+        }
     }
 
 }
