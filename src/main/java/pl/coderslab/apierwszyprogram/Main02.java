27ac2afefd0bedd7618c6baa2af3b2655dbbce96
Maciej Korczala <maciej.coders@gmail.com>
2021-03-25 20:15:45 +0100

    aggregate commit - unstaged changes

--- src/main/java/pl/coderslab/apierwszyprogram/Main02.java
+++ src/main/java/pl/coderslab/apierwszyprogram/Main02.java
@@ -4,5 +4,6 @@ public class Main02 {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Hello Coderslab");
+        //zmiana
     }
 }
